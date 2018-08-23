a71af6de20917d513bd68b6d2c12f818feeee40a
padmapriyanitt <padmapriyanitt@gmail.com>
2018-08-23 10:30:15 +0530

    Removed build, added dist

--- src/components/nav-container/index.js
+++ src/components/nav-container/index.js
@@ -1,8 +1,7 @@
 import { h, Component } from "preact";
 import style from "./style.scss";
-import dpLogo from "../../assets/images/dp-logo.png"
 import {fa} from "font-awesome/css/font-awesome.css"
-import classNames from 'classnames';
+import classNames from "classnames";
 
 export default class App extends Component {
 
@@ -11,21 +10,21 @@ export default class App extends Component {
     this.state = {
       collapsed: false
     };
+    this.toggleSideNav = this.toggleSideNav.bind(this);
   }
 
-  toggleSideNav = () => {
+  toggleSideNav() {
     this.setState({
       collapsed: !this.state.collapsed
     });
     console.log('collapsed:', this.state.collapsed);
-    
-  }
+  };
 
   render(props) {
     return (
       <div className={classNames({[style.navContainer]: true, [style.collapsed]: this.state.collapsed})}>
         <div className={style.dpLogo}>
-          <img src={dpLogo}></img>
+          <img src={`../../assets/images/dp-logo.png`}></img>
           <div class={style.titleContainer}>
             <div className={style.hwxText}><span>Hortonworks</span></div>
             <div className={style.dpText}><span>DataPlane</span></div>
@@ -36,10 +35,11 @@ export default class App extends Component {
           <li><a><i className="fa fa-users"></i><span className={style.menuItemText}>Users</span></a></li>
           <li><a><i className="fa fa-server"></i><span className={style.menuItemText}>Services</span></a></li>
         </ul>
-        <div onClick={this.toggleSideNav} className={style.expander}>
+        <div onClick={this.toggleSideNav}  className={style.expander}>
           {this.state.collapsed?<i className="fa fa-angle-double-right"></i>:<i className="fa fa-angle-double-left"></i>}
         </div>
       </div>
     );
   }
 }
+
